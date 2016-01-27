a9a0152072f9d5dc48310526903caec018d9d104
Artyom Arzamasov <Sorok7@gmail.com>
2016-01-27 11:53:29 +0200

    add theme

--- InstallDots.sh
+++ InstallDots.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-apt-get update && apt-get install git-core zsh -y
+apt-get update && apt-get install  zsh -y
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 cp $DIR/.zshrc ~./ -f
