5e28c520ee3c3aa0806e47b7a6d1044e1d53f39f
Artyom Arzamasov <Sorok7@gmail.com>
2016-01-27 13:24:31 +0200

    Update InstallDots.sh

--- InstallDots.sh
+++ InstallDots.sh
@@ -3,5 +3,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 apt-get update && apt-get install  zsh -y
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-cp $DIR/.zshrc ~./ -f
-cp $DIR/.bash_aliases ~./ -f
+cp $DIR/.zshrc ~/ -f
+cp $DIR/.bash_aliases ~/ -f
