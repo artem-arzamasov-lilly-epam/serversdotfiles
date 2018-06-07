452bb6f9db88a896b81762f3a66f1dcbd0009f7c
Artyom Arzamasov <sorok7@gmail.com>
2018-06-07 20:53:52 +0200

    check

--- InstallDots.sh
+++ InstallDots.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 apt-get update && apt-get install zsh curl -y
 
 cp $DIR/.bash_aliases ~/ -f
-cp $DIR/.vimrc ~/ -f
+cp $DIR/.vimrc ~/ -f 
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp $DIR/.zshrc ~/ -f
