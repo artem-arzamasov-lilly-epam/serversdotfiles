924a0bb1896383b966c14d1adec75990ea5bcc32
Artyom Arzamasov <Sorok7@gmail.com>
2019-03-18 11:37:24 +0100

    Update InstallDots.sh

--- InstallDots.sh
+++ InstallDots.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 ##DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd /tmp
-apt-get update && apt-get install zsh curl -y
+apt-get update && apt-get install zsh curl unzip -y
+## add support for colorized logging
+wget https://github.com/tstack/lnav/releases/download/v0.8.4/lnav-0.8.4-linux-64bit.zip
+unzip lnav-0.8.4-linux-64bit.zip
+mv ./lnav/lnav /usr/bin
+
+wget https://github.com/tstack/lnav/releases/download/v0.8.4/lnav-0.8.4-linux-64bit.zip
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/aarzamasov/serversdotfiles.git
 cd /tmp
